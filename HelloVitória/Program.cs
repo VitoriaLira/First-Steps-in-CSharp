01786b16f83841b086c198ba723a3b3d6d421547
Vitória Carla Costa de Lira <assessoriavitorialira@gmail.com>
2021-09-15 15:35:52 -0300

    Adicionar arquivos de projeto.

---
+++ HelloVitória/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloVitória
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Vitória!!!");
+        }
+    }
+}
